ceabc12b705962549a18b5fc6602bf8bd0d31994
alkabeenakrb-coder <alkabeena.krb@gmail.com>
2026-08-11 17:29:15 +0300

    Delete start_python.bat

--- start_python.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title BioMed Hub - Python Server
-cd /d "%~dp0"
-echo Starting BioMed Hub (Python) on http://localhost:5000
-echo Admin: http://localhost:5000/admin.html
-py server.py
-pause
