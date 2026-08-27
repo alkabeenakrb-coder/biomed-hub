5f863db3c9e56a706627a71548b1727463da00a9
alkabeenakrb-coder <alkabeena.krb@gmail.com>
2026-08-27 04:12:35 +0300

    Delete start_node.bat

--- start_node.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title BioMed Hub - Node Server
-cd /d "%~dp0"
-echo Starting BioMed Hub (Node.js) on http://localhost:8080
-echo Admin: http://localhost:8080/admin.html
-node server.js
-pause
